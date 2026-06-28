d861aa3c8156f8e9b7bfc81aa3adcdd34d82e3ea
香辣鸡腿堡 <1486709979@qq.com>
2026-06-28 23:26:56 +0800

    26.6.28

--- src/main/java/org/albedo/vllmpt/module/ai/service/RerankModelFactory.java
+++ src/main/java/org/albedo/vllmpt/module/ai/service/RerankModelFactory.java
@@ -1,7 +1,9 @@
 package org.albedo.vllmpt.module.ai.service;
 
 import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.cohere.CohereScoringModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.scoring.ScoringModel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -11,22 +13,23 @@ import java.util.concurrent.ConcurrentHashMap;
 @Component
 public class RerankModelFactory {
 
-    @Value("${langchain4j.open-ai.reranker-model.base-url}")
+    @Value("${langchain4j.open-ai.chat-model.base-url}")
     private String baseUrl;
 
-    @Value("${langchain4j.open-ai.reranker-model.api-key}")
+    @Value("${langchain4j.open-ai.chat-model.api-key}")
     private String apiKey;
 
-    private final Map<String, ChatModel> modelCache = new ConcurrentHashMap<>();
 
-    public ChatModel createModel(String modelName) {
+    private final Map<String, ScoringModel> modelCache = new ConcurrentHashMap<>();
+
+    public ScoringModel createModel(String modelName) {
 
 
         String cacheKey = String.format("%s:%.1f:%d", modelName);
 
-        return modelCache.computeIfAbsent(cacheKey, k -> OpenAiChatModel.builder()
-                .baseUrl(baseUrl)
+        return modelCache.computeIfAbsent(cacheKey, k ->  CohereScoringModel.builder()
                 .apiKey(apiKey)
+                .baseUrl(baseUrl)
                 .modelName(modelName)
                 .build());
     }
