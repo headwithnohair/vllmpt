4c1bc686e8015c8691731652a42e9ec8a65bdfca
Albedo_woker1 <1486709979@qq.com>
2026-06-30 18:01:53 +0800

    26.6.30

---
+++ src/main/java/org/albedo/vllmpt/module/chat/controller/EmbeddingTextController.java
@@ -0,0 +1,14 @@
+package org.albedo.vllmpt.module.chat.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class EmbeddingTextController {
+
+    @Autowired
+
+}
